ec0ea13847e06ee1b50f5b5f60f9bdc0066bb073
Scanf-s <calzone0404@gmail.com>
2024-10-09 03:20:32 +0900

    rename

--- Brightness&Arithmetic/convert_color.py
+++ Brightness&Arithmetic/convert_color.py
@@ -5,8 +5,9 @@ if img1 is None:
     print("Image load failed!")
     exit()
 
-img2 = cv.cvtColor(img1)
+img2 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
 
-cv.imshow("img", img1)
+cv.imshow("img1", img1)
+cv.imshow("img2", img2)
 cv.waitKey()
 cv.destroyAllWindows()
